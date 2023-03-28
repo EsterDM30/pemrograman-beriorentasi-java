f36404593bbbae12a317c67109081986a7917e23
EsterDM30 <129130168+EsterDM30@users.noreply.github.com>
2023-03-28 10:03:31 +0700

    Add files via upload

---
+++ angel.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author C-14
+ */
+public class angel {
+    public static void main(String[] args) {      int nilaiA = 10 ;
+      System.out.println("nilai a : "+ nilaiA);
+      Scanner inputSaya = new Scanner( System.in);
+      int nilaiB = 20 ;
+      System.out.println("nilai b :" + nilaiB);
+      double joni= 10.5 ;
+      System.out.println("nilai :" + joni) ;
+      double jono= 30.7 ;
+      System.out.println("nilai: "+ jono );
+              
+      boolean isplus  ;
+      isplus = true;
+      System.out.println("is Plus:" + isplus);
+     
+
+       
+      
+      
+      
+        
+    }
+ 
+}
